1088222a08647f2fd896bf20893ffe1648885d12
pusteugene <pustoviteugene@gmail.com>
2023-10-22 22:23:46 +0300

    add class DVD

--- src/main/java/DVD.java
+++ src/main/java/DVD.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DVD {
+public class DVD extends Item {
+    private int duration;
+
+    public DVD(String title, String uniqueID, int duration) {
+        super(title, uniqueID);
+        this.duration = duration;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (!isBorrowed()) {
+            setBorrowed(true);
+            System.out.println("DVD '" + getTitle() + "' has been borrowed by a patron.");
+        } else {
+            System.out.println("DVD '" + getTitle() + "' is already borrowed.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        if (isBorrowed()) {
+            setBorrowed(false);
+            System.out.println("DVD '" + getTitle() + "' has been returned to the library.");
+        } else {
+            System.out.println("DVD '" + getTitle() + "' is not borrowed and cannot be returned.");
+        }
+    }
 }

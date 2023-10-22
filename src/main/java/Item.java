ceb0e18595c699ebb99712ea07afd1e66c2696fc
pusteugene <pustoviteugene@gmail.com>
2023-10-22 22:24:14 +0300

    add abstract class Item

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Item {
+public abstract class Item {
+    private String title;
+    private String uniqueID;
+    private boolean isBorrowed;
+
+    public Item(String title, String uniqueID) {
+        this.title = title;
+        this.uniqueID = uniqueID;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    public abstract void borrowItem();
+    public abstract void returnItem();
 }

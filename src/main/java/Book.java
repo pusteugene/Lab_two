52eec522b5a7bf72acd7d7dacc0f7cf49b0918d3
pusteugene <pustoviteugene@gmail.com>
2023-10-22 22:23:35 +0300

    add class Book

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Book {
+public class Book extends Item {
+    private String author;
+
+    public Book(String title, String uniqueID, String author) {
+        super(title, uniqueID);
+        this.author = author;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (!isBorrowed()) {
+            setBorrowed(true);
+            System.out.println("Book '" + getTitle() + "' has been borrowed by a patron.");
+        } else {
+            System.out.println("Book '" + getTitle() + "' is already borrowed.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        if (isBorrowed()) {
+            setBorrowed(false);
+            System.out.println("Book '" + getTitle() + "' has been returned to the library.");
+        } else {
+            System.out.println("Book '" + getTitle() + "' is not borrowed and cannot be returned.");
+        }
+    }
 }

62beca033a33470a03890fc18dd6c7bb7974b21d
pusteugene <pustoviteugene@gmail.com>
2023-10-22 22:24:51 +0300

    add class Patron

--- src/main/java/Patron.java
+++ src/main/java/Patron.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Patron {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patron {
+    private String name;
+    private String ID;
+    private List<Item> borrowedItems;
+
+    public Patron(String name, String ID) {
+        this.name = name;
+        this.ID = ID;
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public List<Item> getBorrowedItems() {
+        return borrowedItems;
+    }
+
+    public void borrow(Item item) {
+        // Додавання предмету до списку взятих клієнтом
+        borrowedItems.add(item);
+    }
+
+    public void returnItem(Item item) {
+        // Видалення предмету зі списку взятих клієнтом
+        borrowedItems.remove(item);
+    }
 }
